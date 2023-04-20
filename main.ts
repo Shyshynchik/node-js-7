e1e680c56828390032972e11dfba99983e0f0e12
Igor <igor.kostenko.1999@mail.ru>
2023-04-20 17:06:38 +0300

    сделал прослушку для метода get и post

--- main.ts
+++ main.ts
@@ -6,33 +6,85 @@ const hostname = '172.27.0.2';
 const port = 3000;
 
 const METHOD_GET = 'GET';
-const REDIRECT_URL = 'http://localhost:3001/get'
+const METHOD_POST = 'POST';
+const REDIRECT_URL = 'http://localhost:3001'
 
-const server = http.createServer(function (req: IncomingMessage, res: ServerResponse) {
-    if (req.method == METHOD_GET && req.url == '/get') {
-        http.get(REDIRECT_URL, (response: IncomingMessage) => {
-            if (response.statusCode === undefined) {
-                res.statusCode = 400;
-            } else {
-                res.statusCode = response.statusCode;
-            }
+const REDIRECT_HOST = 'localhost';
+const REDIRECT_PORT = 3001;
 
-            res.setHeader('Content-Type', 'text/plain');
+const methodNotAllowed = function (res: ServerResponse) {
+    res.statusCode = 405;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('405 Method Not Allowed!');
+}
 
-
-            response.on('data', (data: any) => {
-                res.end(data.toString());
-            });
-            return;
-        }).on('error', (error: Error) => {
-            console.error(error);
-        });
-        return;
+const handleResponse = function (res: ServerResponse, response: IncomingMessage) {
+    if (response.statusCode === undefined) {
+        res.statusCode = 400;
+    } else {
+        res.statusCode = response.statusCode;
     }
 
-    res.statusCode = 400;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Bad Gateway!');
+
+
+    response.on('data', (data: any) => {
+        res.end(data.toString());
+    });
+}
+
+const server = http.createServer(function (req: IncomingMessage, res: ServerResponse) {
+    switch (req.url) {
+        case '/get':
+            if (req.method !== METHOD_GET) {
+                methodNotAllowed(res);
+                break;
+            }
+            http.get(REDIRECT_URL + req.url, (response: IncomingMessage) => {
+                handleResponse(res, response);
+                return;
+            }).on('error', (error: Error) => {
+                console.error(error);
+            });
+            break;
+        case '/post':
+            if (req.method !== METHOD_POST) {
+                methodNotAllowed(res);
+                break;
+            }
+
+            const postOptions = {
+                host: REDIRECT_HOST,
+                port: REDIRECT_PORT,
+                path: '/post',
+                method: METHOD_POST,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Content-Length': 0
+                }
+            };
+
+            const response = http.request(postOptions, (_res: any) => {
+                let message = '';
+
+                _res.on('data', (chunk: any) => {
+                    message += chunk;
+                });
+
+                _res.on('end', () => {
+                    res.statusCode = _res.statusCode;
+                    res.setHeader('Content-Type', 'text/plain');
+                    res.end(message);
+                });
+            });
+
+            response.end();
+            break;
+        default:
+            res.statusCode = 400;
+            res.setHeader('Content-Type', 'text/plain');
+            res.end('Bad Gateway!');
+    }
 });
 
 server.listen(port, hostname, () => {
--- second.ts
+++ second.ts
@@ -6,18 +6,40 @@ const hostname = 'localhost';
 const port = 3001;
 
 const METHOD_GET = 'GET';
+const METHOD_POST = 'POST';
+
+const methodNotAllowed = function (res: ServerResponse) {
+    res.statusCode = 405;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('405 Method Not Allowed!');
+    return;
+}
 
 const server = http.createServer(function (req: IncomingMessage, res: ServerResponse) {
-    if (req.method == METHOD_GET && req.url == '/get') {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Hello, World!');
-        return;
+    switch (req.url) {
+        case '/get':
+            if (req.method !== METHOD_GET) {
+                methodNotAllowed(res);
+                break;
+            }
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/plain');
+            res.end('Hello, World from GET');
+            break;
+        case '/post':
+            if (req.method !== METHOD_POST) {
+                methodNotAllowed(res);
+                return;
+            }
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/plain');
+            res.end('Hello, World from POST');
+            break;
+        default:
+            res.statusCode = 400;
+            res.setHeader('Content-Type', 'text/plain');
+            res.end('Bad Gateway!');
     }
-
-    res.statusCode = 400;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Bad Gateway!');
 });
 
 server.listen(port, hostname, () => {
